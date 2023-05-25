30feba2c333af155c765a33d1035bef910408e18
Louiz <127776817+LouizFeliphe@users.noreply.github.com>
2023-05-25 02:29:53 -0300

    vgdge
    gegeg

--- ex39[/index.js
+++
@@ -1,6 +0,0 @@
-a = -2
-b = 2
-
-let livro = (a == -b);
-
-console.log(livro)
---
+++ index.js
@@ -0,0 +1,6 @@
+function minhaFuncao() {
+    window.minhaVariavel = "Valor global";
+  }
+  
+  minhaFuncao();
+  console.log(minhaVariavel);  // Imprime "Valor global"
